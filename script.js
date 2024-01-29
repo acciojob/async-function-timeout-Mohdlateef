d0901bd0f908bbc541b4ee7968a7efb9ec92ef5e
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-29 22:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let btn=document.getElementById("btn");
+
+let output=document.getElementById("output");
+btn.addEventListener("click",()=>{
+	let text=document.getElementById("text").value;
+let num=document.getElementById("delay").value;
+	setTimeout(()=>{
+		output.innerText=`${text}`
+	},num)
+});
